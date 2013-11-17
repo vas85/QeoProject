86b6d4c59fdfac1f61f6b717547e7561c44daf49
Tushar Dadlani <tush726@gmail.com>
2013-11-17 04:08:23 -0800

    youtube player and qeo integration

---
+++ yt-player/src/com/example/qeohelloworld/ChatMessage.java
@@ -0,0 +1,46 @@
+package com.example.qeohelloworld;
+
+public class ChatMessage {
+	    /**
+	     * The user sending the message.
+	     */
+	    public String from;
+
+	    /**
+	     * The message.
+	     */
+	    public String message;
+
+	    public ChatMessage()
+	    {
+	    }
+
+	    @Override
+	    public boolean equals(Object obj)
+	    {
+	        if (obj == this) {
+	            return true;
+	        }
+	        if ((obj == null) || (obj.getClass() != this.getClass())) {
+	            return false;
+	        }
+	        final ChatMessage myObj = (ChatMessage)obj;
+	        if (!from.equals(myObj.from)) {
+	            return false;
+	        }
+	        if (!message.equals(myObj.message)) {
+	            return false;
+	        }
+	        return true;
+	    }
+
+	    @Override
+	    public int hashCode()
+	    {
+	        final int prime = 31;
+	        int result = 1;
+	        result = prime * result + ((from == null) ? 0 : from.hashCode());
+	        result = prime * result + ((message == null) ? 0 : message.hashCode());
+	        return result;
+	    }
+}
